7a060c4a17b3322e8e035a893f086bd20385655f
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2020-08-29 22:39:45 +0800

    Will update insta embed to current

--- js/style.js
+++ js/style.js
@@ -1,3 +1,27 @@
+//
+var instaBodyTopPadding = 28;
+var minWidth = 300;
+var maxWidth = 500;
+// Frame Width size calculation. To call on first load and resize as well.
+function getWidth(element){
+    stringTarget = getComputedStyle(element).width;
+    //Very Lazy match
+    match = stringTarget.match(/^[0-9]*/)[0];
+    return Number(match);
+}
+function sizeInstaFrame () {
+    var containerWidth = getWidth(document.getElementById("instaFrameContainer"));
+
+    if ( containerWidth > maxWidth ) {
+        frameWidth = maxWidth + ( instaBodyTopPadding * 2);
+    } else if ( minWidth <= containerWidth ) {
+        frameWidth = containerWidth + ( instaBodyTopPadding * 2);
+    } else {
+        frameWidth = minWidth + ( instaBodyTopPadding * 2);
+    }
+    document.getElementById("instaFrame").style.height = String(frameWidth) + "px";
+} sizeInstaFrame();
+document.addEventListener("resize", sizeInstaFrame);
 // Quick Action PopUp Styling and Functionality
 var quickAction = document.getElementById("quickAction");
 var quickActionBar = document.getElementById("quickActionBar")
