888f02db46f8ffde45445ab3aec135cfe5ada9d2
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2020-09-05 15:53:30 +0800

    Add deployment files/tools

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+## Simply copies the website files to root directory using rsync
+
+ROOT="/var/www/html/D2G"
+#Files that are not to be deployed
+EXCLUDE="NotToBeDeployed.txt"
+SERVER="root@172.105.188.27:/var/www/html/public_html"
+
+if ! [[ -f "$EXCLUDE" ]]; then
+    echo "Warning, this script might be included in deployment if it is in the same directory."
+fi
+
+if [[ $1 == "server" ]]; then
+    rsync -va -e "ssh" --exclude-from="$EXCLUDE" . "$SERVER"
+else
+    if ! [[ -d "$ROOT" ]]; then
+        mkdir "$ROOT"
+    fi
+    rsync -v -a --exclude-from="$EXCLUDE" . "$ROOT"
+fi
