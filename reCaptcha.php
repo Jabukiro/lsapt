52f3893e7e2016988e0eb5c61c1a9236d14206ad
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2021-07-17 19:17:31 +0200

    Add ability to send dev mail if g-recaptcha-response not set

--- reCaptcha.php
+++ reCaptcha.php
@@ -1,9 +1,22 @@
 <?php
+/**
+ * reCaptcha assesment creation
+ * Assumes it is being called when needed.
+ */
  require 'vendor/autoload.php';
+ $ENV = parse_ini_file( "env.ini" );
  use Google\Cloud\RecaptchaEnterprise\V1\RecaptchaEnterpriseServiceClient;
  use Google\Cloud\RecaptchaEnterprise\V1\Event;
  use Google\Cloud\RecaptchaEnterprise\V1\Assessment;
  use Google\Cloud\RecaptchaEnterprise\V1\TokenProperties\InvalidReason;
+ //HTML Email stuff
+ $headers = "From: admin\r\n";
+ $headers .= "MIME-Version: 1.0\r\n";
+ $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+ $HTMLBEGIN = "<html lang=\"en\"></body>";
+ $HTMLEND = "</body></html>";
+
+ //reCaptcha related
  //$SPAM = false;
  $REASON = "";
  $SCORE = NAN;
@@ -61,4 +74,11 @@
      //Assuming is spam
      $SCORE = NAN;
      $REASON = "g-recaptcha-response was not set";
+     $subject = "LSAPT Website | g-recaptcha-response not set whilst expected";
+     $body = $HTMLBEGIN."<h3>POST content of request: </h3><p>".$result."</p>".$HTMLEND;
+     try{
+        mail($ENV["DEVMAIL"], $subject, $body, $headers);
+     } catch (exception $e) {
+         error_log("Error sending email. Reason: unset POST g-recaptcha-response. EXCEPTION: $result");
+     }
  }
