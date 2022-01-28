6511c0a7002e7d93cae10aa2f97551af57b5ee58
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2022-01-28 23:22:59 +0200

    Move logic in appropriate files. Add loading indicators to page products

--- Front/svelte/src/components/cartContent.svelte
+++ Front/svelte/src/components/cartContent.svelte
@@ -1,133 +1,28 @@
 <script>
-  import { prevent_default } from "svelte/internal";
-
+  export let closeDrawer;
+  //expects a list, whether empty or not.
+  //if not empty then it is a list containing CartProducts
+  export let cartList;
+  export let cartOps;
   import CartProduct from "./cartProductCard.svelte";
-  const openDrawer = () => {
-    const cartDrawer = document.getElementById("cart-drawer");
-    const [backdrop, drawerPaper] = cartDrawer.children;
-    cartDrawer.style.visibility = "visible";
-    backdrop.style.opacity = 0.5;
-    drawerPaper.style.right = 0;
-  };
-  const closeDrawer = () => {
-    const cartDrawer = document.getElementById("cart-drawer");
-    const [backdrop, drawerPaper] = cartDrawer.children;
-    cartDrawer.style.visibility = "hidden";
-    backdrop.style.opacity = 0;
-    drawerPaper.style.right = "-500px";
-  };
-  const getProduct = (id, signal = null) => {
-    fetch("https://lapt.localhost:4000/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        query: `query GetProduct($id: ID!) { getProduct(id:$id) {
-          id
-          name
-          attributes
-          description
-          image
-          fee
-        }}`,
-        variables: { id },
-      }),
-      signal,
-    })
-      .then((r) => r.json())
-      .then((data) => {
-        if (
-          data &&
-          data.data &&
-          typeof data.data.getProduct === "object" &&
-          data.data.getProduct.id != null
-        ) {
-          Products.add(data.data.getProduct);
-          console.log(Products);
-          return;
-        }
-        if (data && data.data && typeof data.data.getProduct === "object") {
-          console.log("No such product in db? Returned data: ", data);
-          //No such product?!?!
-          return;
-        }
-        //Something wrong with data
-        console.log("Something wrong with db? Returned data: ", data);
-      })
-      .catch((e) => {
-        console.log(e);
-        return;
-      });
-  };
-  document
-    .querySelectorAll(".session-product-container .content-actions .btn")
-    .forEach((closer) => {
-      closer.addEventListener("click", (e) => {
-        getProduct(e.target.getAttribute("data-id"));
-      });
-    });
+
+  const loadingIncrement = () => {};
   const backdrop = document.getElementById("cart-drawer").children[0];
   backdrop.addEventListener("click", () => {
     closeDrawer();
   });
-  document.getElementById("cartIcon").addEventListener("click", () => {
-    openDrawer();
-  });
   //Add all these 3 functions to Product Object
   const incrementItem = (id) => {
-    const index = Products.list.findIndex((product) => product.id === id); //Find where the product with corresponding ID is
-    Products.list[index].count += 1;
+    cartOps({ id, type: "add" }); //Server side logic will increment if product already in cart.
   };
   const decrementItem = (id) => {
-    const index = Products.list.findIndex((product) => product.id === id); //Find where the product with corresponding ID is
-    if (Products.list[index].count === 1) {
-      return;
-    }
-    Products.list[index].count -= 1;
+    cartOps({ id, type: "decrement" });
   };
   const removeItem = (id) => {
-    Products.list = Products.list.filter((product) => product.id !== id);
+    cartOps({ id, type: "remove" });
   };
 
-  const Products = {
-    __addFlag: false,
-    list: [],
-    //It is assumed that a single product is added
-    //The list keeps no duplicate products and rather uses count to keep track of how many of each product
-    //This function might be covering an edge case really as there is an incrementing button on cart.
-    add: (product) => {
-      if (Products.list.length === 0) {
-        //Adding first product
-        product.count = 1;
-        Products.push(product);
-        console.log("product added to cart", product);
-        return;
-      }
-      Products.list.map((cartProduct, index) => {
-        console.log("add() considering Cart Product: ", cartProduct);
-        if (product.id === cartProduct.id) {
-          cartProduct.count += 1;
-          console.log("cartProduct count incremented: ", cartProduct);
-          Products.__addFlag = true;
-        }
-        if (Products.list.length === index + 1 && !Products.__addFlag) {
-          product.count = 1;
-          Products.push(product);
-          Products.__addFlag = false;
-          console.log("product added to cart", product);
-        }
-      });
-      console.log("add() processed product: ", product);
-    },
-    //Replicates push behavior for svelte reactivity
-    push: (product) => {
-      Products.list.push(product);
-      Products.list = Products.list;
-    },
-  };
-  $: subTotal = Products.list.reduce((previous, current) => {
+  $: subTotal = cartList.reduce((previous, current) => {
     return previous + current.fee * current.count;
   }, 0);
   //Reserved. Might be usefull if discounts are available.
@@ -137,7 +32,7 @@
 <div class="cart-header">
   <div class="cart-header-main">
     <h2 class="h3">Shopping Cart</h2>
-    <label for="cart-drawer" class="small-font">{Products.list.length}</label>
+    <label for="cart-drawer" class="small-font">{cartList.length}</label>
   </div>
   <button
     on:click={closeDrawer}
@@ -150,7 +45,7 @@
 </div>
 <div class="cart-body-wrapper">
   <div class="cart-body">
-    {#each Products.list as product (product.id)}
+    {#each cartList as product (product.id)}
       <CartProduct
         {product}
         incrementItem={() => incrementItem(product.id)}
@@ -159,7 +54,7 @@
       />
     {/each}
   </div>
-  <div class="cart-checkout" class:displayHide={Products.list.length === 0}>
+  <div class="cart-checkout" class:displayHide={cartList.length === 0}>
     <div class="cart-checkout-totals mb-3">
       <div
         class="cart-checkout-subtotals d-flex flex-row justify-content-between small-font"
--- Front/svelte/src/components/cartDrawer.svelte
+++ Front/svelte/src/components/cartDrawer.svelte
@@ -1,24 +1,201 @@
 <script>
-    import CartContent from "./cartContent.svelte";
+  import CartContent from "./cartContent.svelte";
+  let isOpen = false;
+  const PRODUCTSLOCATION = [
+    "/",
+    "/index.php",
+    "/training",
+    "/training/index.php",
+  ];
+  const pageUrl = new URL(window.location.href);
+  let cartList = [];
+  const cartQuery = ({ onCompletion = null } = {}) => {
+    fetch("https://lapt.localhost:4000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        query: `query Cart { cart {
+          list{
+            id
+            name
+            attributes
+            description
+            image
+            fee
+            count
+          }
+        }}`,
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (
+          data &&
+          data.data &&
+          typeof data.data.cart === "object" &&
+          typeof data.data.cart.list === "object"
+        ) {
+          cartList = data.data.cart.list;
+          console.log(cartList);
+          return;
+        }
+        if (data && data.data && typeof data.data.cart === "object") {
+          console.log("Cart List not object? Returned data: ", data);
+          //No such product?!?!
+          return;
+        }
+        //Something wrong with data
+        console.log("Something wrong with db? Returned data: ", data);
+      })
+      .catch((e) => {
+        console.log(e);
+        return;
+      })
+      .finally(() => {
+        if (onCompletion !== null) {
+          onCompletion();
+          return;
+        }
+      });
+  };
+  cartQuery(); //Fetch cart saved on server if there is any
+  const cartOps = ({
+    id,
+    type,
+    reject = null,
+    resolve = null,
+    onCompletion = null,
+    signal = null,
+  }) => {
+    fetch("https://lapt.localhost:4000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        query: `mutation CartOperations($input: CartOperationsInput!) { cartOperations(input:$input) {
+          list{
+            id
+            name
+            attributes
+            description
+            image
+            fee
+            count
+          }
+        }}`,
+        variables: { input: { id, type } },
+      }),
+      signal,
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (
+          data &&
+          data.data &&
+          typeof data.data.cartOperations === "object" &&
+          typeof data.data.cartOperations.list === "object"
+        ) {
+          cartList = data.data.cartOperations.list;
+          console.log(cartList);
+          return;
+        }
+        if (data && data.data && typeof data.data.cartOperations === "object") {
+          console.log("Cart List not object? Returned data: ", data);
+          //No such product?!?!
+          return;
+        }
+        //Something wrong with data
+        console.log("Something wrong with db? Returned data: ", data);
+      })
+      .catch((e) => {
+        console.log(e);
+        return;
+      })
+      .finally(() => {
+        if (onCompletion !== null) {
+          onCompletion();
+          return;
+        }
+      });
+  };
+  const openDrawer = () => {
+    document.getElementById("cart-drawer").classList.add("open");
+  };
+  const closeDrawer = () => {
+    document.getElementById("cart-drawer").classList.remove("open");
+  };
+  document.getElementById("cartIcon").addEventListener("click", () => {
+    openDrawer();
+  });
+  const loadingCart = (id, state, productEl) => {
+    switch (state) {
+      case "start":
+        productEl.classList.add("loading");
+        return;
+      case "stop":
+        productEl.classList.remove("loading");
+        return;
+      default:
+        console.log(
+          "loadingCart expected 'state' parameter to be one of 'start' or 'stop'. Instead state is: ",
+          state
+        );
+        break;
+    }
+  };
+  if (PRODUCTSLOCATION.some((location) => location === pageUrl.pathname)) {
+    //Lets load the products that are displayed on the page
+    //Needed so that they can be target by loading indicators
+    const domProducts = Array.from(
+      document.querySelectorAll(".session-product-container")
+    );
+
+    domProducts.forEach((productEl) => {
+      productEl
+        .querySelector(".content-actions .btn") //the Add To Cart button
+        .addEventListener("click", (e) => {
+          const id = e.target.getAttribute("data-id");
+          loadingCart(id, "start", productEl);
+          cartOps({
+            id,
+            type: "add",
+            onCompletion: () => {
+              loadingCart(id, "stop", productEl);
+              openDrawer();
+            },
+          });
+        });
+    });
+  }
 </script>
 
-<div class="cart-paper">
-    <CartContent />
+<div class="cart-paper" class:open={isOpen === true}>
+  <CartContent {cartOps} {cartList} {closeDrawer} />
 </div>
 
 <style>
-    .cart-paper {
-        width: 500px;
-        height: 100%;
-        max-width: 100vw;
-        position: fixed;
-        right: -500px; /**Will use right positioning to move cart in and out of plane*/
-        top: 0;
-        z-index: 3000;
-        transition-property: right;
-        transition-timing-function: inherit;
-        transition-duration: inherit;
-        background-color: white;
-        overflow-x: hidden;
-    }
+  .cart-paper {
+    width: 500px;
+    height: 100%;
+    max-width: 100vw;
+    position: fixed;
+    right: -500px; /**Will use right positioning to move cart in and out of plane*/
+    top: 0;
+    z-index: 3000;
+    transition-property: right;
+    transition-timing-function: inherit;
+    transition-duration: inherit;
+    background-color: white;
+    overflow-x: hidden;
+  }
+  .open.cart-paper {
+    right: 0px;
+  }
 </style>
