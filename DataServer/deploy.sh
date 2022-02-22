9e5321243be87f6cc4e364c6af083bc37fe93ec6
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2022-02-22 12:09:18 +0200

    DEV: Add separate deployment for Data Server

---
+++ DataServer/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## Simply copies the website files to root directory using rsync
+
+#Files that are not to be deployed
+EXCLUDE="NotToBeDeployed.txt"
+SERVER="jabukiro@172.105.188.27:~/DataServer"
+ERRORSTRING="Error. Please make sure you've indicated correct parameters"
+if [ $# -eq 0 ]
+    then
+        echo $ERRORSTRING;
+elif [ $1 == "live" ]
+    then
+        if [[ -z $2 ]]
+            then
+                echo "Running dry-run"
+                rsync --dry-run -az --no-perms --force --delete --progress --exclude-from="$EXCLUDE"  ./ "$SERVER"
+        elif [ $2 == "go" ]
+            then
+                echo "Running actual deploy"
+                rsync -az --no-perms --force --delete --progress --exclude-from="$EXCLUDE"  ./ "$SERVER"
+        else
+            echo $ERRORSTRING;
+        fi
+fi
