d6fff21e88eb329528197a0d2f5d6f2d03971708
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2022-04-04 16:35:24 +0200

    Add session registration handling capabilities

--- DataServer/cartProduct.js
+++ DataServer/cartProduct.js
@@ -1,3 +1,5 @@
+const session = require("express-session");
+
 exports.add = (newProduct, productList) => {
     let addFlag = false;
     if (productList.length === 0) {
@@ -19,6 +21,22 @@ exports.add = (newProduct, productList) => {
     });
     return productList;
 }
+exports.addSession = (newSession, sessionList) => {
+    //If there is a sessionRegistration for a particular sessionID
+    //Overwrite with new sessionRegistration
+    //Done in case an athlete needs
+    console.log("cartProduct.js: sessionList", JSON.stringify(sessionList));
+    for (let i = 0; i < sessionList.length; i++) {
+        if (newSession.session.id == sessionList[i].session.id) {
+            sessionList[i] = newSession;
+            return sessionList;
+        }
+    }
+    //Covers the case where the particular sessionID isn't present
+    //or sessionList is empty
+    sessionList.push(newSession)
+    return sessionList;
+}
 exports.decrementItem = (id, productList) => {
     const index = productList.findIndex((product) => product.id == id); //Find where the product with corresponding ID is
     if (index === -1) { return productList; } // no such product in cart
@@ -29,6 +47,12 @@ exports.decrementItem = (id, productList) => {
     productList[index].count -= 1; //decrement
     return productList;
 };
+getProducts = (cartSession) => {
+    return typeof cartSession.value === "object" ? cartSession.value : [];
+}
+getRegisteredSessions = (cartSession) => {
+    return typeof cartSession.registeredSessions === "object" ? cartSession.registeredSessions : [];
+}
 exports.removeItem = (id, productList) => {
     return productList.filter((product) => product.id != id);
 }
